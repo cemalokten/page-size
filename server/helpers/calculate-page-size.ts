16d9d93bad013fbf9b44243fcca69f23de0d8099
cemalokten <cemalokten@gmail.com>
2023-09-12 14:30:58 +0100

    Update calculate page-size

--- server/helpers/calculate-page-size.ts
+++ server/helpers/calculate-page-size.ts
@@ -1,3 +1,4 @@
+import axios from "axios";
 import { get_assets } from "./get-assets.js";
 import { get_size } from "./get-size.js";
 
@@ -5,29 +6,25 @@ export async function calculate_total_size(url: string): Promise<number> {
   try {
     const url_assets: Set<string> = new Set();
     const other_assets: Set<number> = new Set();
-
-    const res: Response = await fetch(url);
-
-    if (!res.ok) {
-      throw new Error(`Failed to fetch URL ${url} with status: ${res.status}`);
-    }
-
-    const base_url = new URL(res.url).origin;
-    const html_content = await res.text();
-
     url_assets.add(url);
 
-    get_assets(html_content, url_assets, other_assets, base_url);
+    const res = await axios.get(url);
+    const base_url = new URL(res.config.url as string).origin;
+
+    get_assets(res, url_assets, other_assets, base_url);
 
     const array_of_sizes = [...url_assets].map((url) => get_size(url));
+    const sizes: PromiseSettledResult<number>[] =
+      await Promise.allSettled(array_of_sizes);
 
-    const sizes = await Promise.all(array_of_sizes);
-
-    const total_size = sizes.reduce((a, c) => a + Number(c), 0);
+    const total_size = sizes.reduce((a, c) => {
+      if (c.status === "fulfilled") {
+        return a + Number(c.value);
+      }
+      return a;
+    }, 0);
     const other_size = [...other_assets].reduce((a, c) => a + c, 0);
-
     const total = Math.floor((total_size + other_size) / 1024);
-
     return total;
   } catch (error) {
     console.error(error);
