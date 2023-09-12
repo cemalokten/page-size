e2ed2a72b4f87b9e296a0a3c85c3ef244c0e08b0
cemalokten <cemalokten@gmail.com>
2023-09-12 14:31:37 +0100

    Update get assets
    Add finally which ensures that JSDOM does not leak memory

--- server/helpers/get-assets.ts
+++ server/helpers/get-assets.ts
@@ -2,87 +2,95 @@ import jsdom from "jsdom";
 import { add_url } from "./add-url";
 import { extract_background_URL } from "./extract-background-url";
 import { add_other_sizes } from "./add-other-sizes";
+import { AxiosResponse } from "axios";
 
 export function get_assets(
-  html_content: string,
+  res: AxiosResponse,
   url_assets: Set<String>,
   other_assets: Set<Number>,
   base_url: string,
 ) {
-  const { JSDOM } = jsdom;
-  const virtualConsole = new jsdom.VirtualConsole();
+  let DOM;
 
-  const DOM = new JSDOM(html_content, { virtualConsole });
+  try {
+    const { JSDOM } = jsdom;
+    const virtualConsole = new jsdom.VirtualConsole();
 
-  const pictures = new Set([
-    ...DOM.window.document.querySelectorAll("picture"),
-  ]);
-  const images = new Set([
-    ...DOM.window.document.querySelectorAll(":not(picture) > img"),
-  ]);
-  const backgroundImages = new Set([
-    ...DOM.window.document.querySelectorAll('[style*="background-image"]'),
-  ]);
-  const favicons = new Set([
-    ...DOM.window.document.querySelectorAll("link[rel~='icon']"),
-  ]);
-  const scripts = new Set([...DOM.window.document.querySelectorAll("script")]);
+    DOM = new JSDOM(res.data, { virtualConsole });
 
-  const preloadedScripts = [
-    ...DOM.window.document.querySelectorAll('link[rel="defer"][as="script"]'),
-  ];
+    const pictures = new Set([
+      ...DOM.window.document.querySelectorAll("picture"),
+    ]);
+    const images = new Set([
+      ...DOM.window.document.querySelectorAll(":not(picture) > img"),
+    ]);
+    const backgroundImages = new Set([
+      ...DOM.window.document.querySelectorAll('[style*="background-image"]'),
+    ]);
+    const favicons = new Set([
+      ...DOM.window.document.querySelectorAll("link[rel~='icon']"),
+    ]);
+    const scripts = new Set([
+      ...DOM.window.document.querySelectorAll("script"),
+    ]);
 
-  console.log(preloadedScripts);
+    if (pictures.size > 0) {
+      for (let picture of pictures) {
+        const source = picture.querySelectorAll("source");
+        const src = source[0].srcset.split(" ")[0];
+        if (src && src.trim() !== "") {
+          add_url({ src, base_url, url_assets });
+        }
+      }
+    }
 
-  if (pictures.size > 0) {
-    for (let picture of pictures) {
-      const source = picture.querySelectorAll("source");
-      const src = source[0].srcset.split(" ")[0];
-      if (src && src.trim() !== "") {
+    if (images.size > 0) {
+      for (const image of images) {
+        const src = image.getAttribute("src");
+        if (src && src.trim() !== "") {
+          add_url({ src, base_url, url_assets });
+        }
+      }
+    }
+
+    if (backgroundImages.size > 0) {
+      for (const backgroundImage of backgroundImages) {
+        const style = backgroundImage.getAttribute("style");
+        const src = extract_background_URL(style);
+        if (src && src.trim() !== "") {
+          add_url({ src, base_url, url_assets });
+        }
+      }
+    }
+
+    for (const icon of favicons) {
+      const href = icon.getAttribute("href");
+      if (href?.startsWith("data")) {
+        const base64 = icon.getAttribute("href")?.split(",")[1];
+        if (base64) {
+          const total_size_bytes = (base64.length * 6 * 8) / 8;
+          add_other_sizes(total_size_bytes, other_assets);
+        }
+      } else if (href !== null) {
+        const src = href;
         add_url({ src, base_url, url_assets });
       }
     }
-  }
 
-  if (images.size > 0) {
-    for (const image of images) {
-      const src = image.getAttribute("src");
-      if (src && src.trim() !== "") {
-        add_url({ src, base_url, url_assets });
+    if (scripts.size > 0) {
+      for (const script of scripts) {
+        const src = script.getAttribute("src");
+        if (src && src.trim() !== "") {
+          add_url({ src, base_url, url_assets });
+        }
       }
     }
-  }
-
-  if (backgroundImages.size > 0) {
-    for (const backgroundImage of backgroundImages) {
-      const style = backgroundImage.getAttribute("style");
-      const src = extract_background_URL(style);
-      if (src && src.trim() !== "") {
-        add_url({ src, base_url, url_assets });
-      }
+  } catch (error) {
+    console.error("Error while processing the DOM");
+  } finally {
+    if (DOM && DOM.window) {
+      DOM.window.close();
     }
-  }
-
-  for (const icon of favicons) {
-    const href = icon.getAttribute("href");
-    if (href?.startsWith("data")) {
-      const base64 = icon.getAttribute("href")?.split(",")[1];
-      if (base64) {
-        const total_size_bytes = (base64.length * 6 * 8) / 8;
-        add_other_sizes(total_size_bytes, other_assets);
-      }
-    } else if (href !== null) {
-      const src = href;
-      add_url({ src, base_url, url_assets });
-    }
-  }
-
-  if (scripts.size > 0) {
-    for (const script of scripts) {
-      const src = script.getAttribute("href");
-      if (src && src.trim() !== "") {
-        add_url({ src, base_url, url_assets });
-      }
-    }
+    DOM = null;
   }
 }
